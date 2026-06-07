8cb0d5de11a0ae91c142be83782660047e5bc753
Aditya Sharma <sharmaaditya200604@gmail.com>
2026-06-08 00:00:56 +0530

    Learn about Code execution in system

---
+++ Lectures/DAY 1/04_cprogram.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main(){
+    printf("hello world");
+    }
+
+
+
+// EXECUTION PROCESS OF C 
+
+                     //       Source Code (first.c)
+                     //      Preprocessor (first.i)
+                     //      Compileṇ
+                     //      syntax error ---yes --- back to source code
+                     //   --- no--- then assembler(first.amy)
+                     //         fisrt.obj 
+                     //      link with sys libraries
+                     //            first.exe
+                     //               execute
